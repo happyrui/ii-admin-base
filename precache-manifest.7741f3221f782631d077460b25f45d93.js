c0237c46765204b0bad2ca5603a5cc90bb86e8e6
Bian2017 <bgl_cumt_zju@163.com>
2020-06-20 09:50:11 +0000

    Updates

---
+++ precache-manifest.7741f3221f782631d077460b25f45d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4fee25b6676bfe0027b5e272401008",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9f94eace9447313723d8.bundle.js"
+  },
+  {
+    "url": "runtime~main.9f94eace9447313723d8.bundle.js"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.9f94eace9447313723d8.bundle.js"
+  },
+  {
+    "url": "vendors~main.9f94eace9447313723d8.bundle.js.LICENSE.txt"
+  }
+]);
