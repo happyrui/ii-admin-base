d5447166fd5cdbc3f675f1879786ddacb92b001a
Travis <travis@travis-ci.com>
2020-06-27 07:50:58 +0000

    Updates

---
+++ precache-manifest.6c322ead10bb67b2756bb36057eae3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f8d97163d9cb91a81466ee2b126149",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3834e4d707b035a76019.bundle.js"
+  },
+  {
+    "url": "runtime~main.3834e4d707b035a76019.bundle.js"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.3834e4d707b035a76019.bundle.js"
+  },
+  {
+    "url": "vendors~main.3834e4d707b035a76019.bundle.js.LICENSE.txt"
+  }
+]);
